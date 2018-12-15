56d51182334e0820ab59bfb5a2b1b0274302e9cf
Callum Wallace <36588838+CalWal@users.noreply.github.com>
2018-12-15 20:32:44 +1000

    Changed App class component
    added TodoData to state

--- src/App.js
+++ src/App.js
@@ -3,20 +3,21 @@ import React from "react";
 import NavBar from "/components/NavBar";
 import MainContent from "/components/MainContent";
 import Footer from "/components/Footer";
-import Todo from "/components/Todo";
 import TodoItem from "/components/TodoItem";
 import TodoData from "./TodoData.js";
 
-const App = () => {
-  const TodoComponent = TodoData.map(todo => (
-    <TodoItem
-      todo={todo}
-      key={todo.id}
-      text={todo.text}
-      completed={todo.completed}
-    />
-  ));
-  return <div>{TodoComponent}</div>;
-};
-
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: TodoData
+    };
+  }
+  render() {
+    const TodoComponent = this.state.todos.map(todo => (
+      <TodoItem todo={todo} key={todo.id} text={todo.text} />
+    ));
+    return <div>{TodoComponent}</div>;
+  }
+}
 export default App;
